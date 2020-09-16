d2435de3232773c62a681217e1d32e78f05c18c3
Alerson <alerson.rigo@gmail.com>
2020-09-16 09:40:17 -0300

    gerando info

--- src/main/java/com/swagger/config/spring/config/SpringFoxConfig.java
+++ src/main/java/com/swagger/config/spring/config/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.swagger.config.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,12 +18,22 @@ public class SpringFoxConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
+		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.swagger.config.rest.controller"))
 				.paths(PathSelectors.ant("/api/*"))
 				.build()
-				.useDefaultResponseMessages(false);
+				.useDefaultResponseMessages(false)
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Simple Spring Boot REST API")
+				.description("Um exemplo de aplicação Spring Boot REST API com SpringFox para documentar")
+				.version("1.0.0")
+				.license("Apache License Version 2.0")
+				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+				.contact(new Contact("Alerson Rigo", "https://github.com/Alerson/swagger-config", "alerson.rigo@gmail.com"))
+				.build();
 	}
 
 }
